83a02d8588613f74692d38b5cf45b99d5eb234a0
MPhuongLe <mphuongle30@gmail.com>
2024-01-07 16:50:07 +0700

    finish item request

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ load_dotenv()
 from flask import Flask, render_template
 from tables.user import user_blueprint
 from tables.trip import trip_blueprint
+from tables.item import item_blueprint
+from tables.album import album_blueprint
 from tables.photo import photo_blueprint
 
 app = Flask(__name__)
 
 app.register_blueprint(user_blueprint, url_prefix='/user')
 app.register_blueprint(trip_blueprint, url_prefix='/trip')
+app.register_blueprint(item_blueprint, url_prefix='/item')
+app.register_blueprint(album_blueprint, url_prefix='/album')
 app.register_blueprint(photo_blueprint, url_prefix='/photo')
 
 @app.route('/')
