68b230f71035581ddcf09ba1da9e5cd7511e5bc4
MPhuongLe <mphuongle30@gmail.com>
2024-01-18 11:07:12 +0700

    Add api: Insert planned destination

--- source/tables/planneddestination.py
+++ source/tables/planneddestination.py
@@ -51,30 +51,37 @@ def get_planned_destinations_by_tripid():
 # Insert planned_destination 
 @planned_destination_blueprint.route('/insert_planned_destination', methods=['POST'])
 def insert_planned_destination():
+    print('haha')
     try:
         data = request.get_json()
-        userid = data.get('userid');
-        name = data.get('name');
-        departureplace = data.get('departureplace');
-        arrivalplace = data.get('arrivalplace');
-        departuredate = data.get('departuredate');
-        arrivaldate = data.get('arrivaldate');
+        tripid = data.get('tripid');
+        locationid = data.get('locationid');
+        begintime = data.get('begintime');
+        endtime = data.get('endtime');
+        note = data.get('note');
 
-        if not userid or not name or not departureplace or not arrivalplace or not departuredate or not arrivaldate:
+        if not tripid or not locationid:
             return jsonify({"error": "Missing required parameters"}), 400
-
+        print('new')
         planned_destination_data = {}
-        planned_destination_data['userid'] = userid
-        planned_destination_data['name'] = name
-        planned_destination_data['arrivalplace'] = arrivalplace
 
-        if departureplace:
-            planned_destination_data['departureplace'] = departureplace
-        if departuredate:
-            planned_destination_data['departuredate'] = departuredate
-        if arrivaldate:
-            planned_destination_data['arrivaldate'] = arrivaldate
+        planned_destination_data['tripid'] = tripid
 
+        current_destinations_response = supabase.table("planneddestination").select("*").eq("tripid", tripid).order('destinationno', desc=True).execute()
+        print(current_destinations_response)
+        max_destination_no = current_destinations_response.data[0]['destinationno']
+
+        planned_destination_data['destinationno'] = max_destination_no + 1
+        planned_destination_data['locationid'] = locationid
+
+        if begintime:
+            planned_destination_data['begintime'] = begintime
+        if endtime:
+            planned_destination_data['endtime'] = endtime
+        if note:
+            planned_destination_data['note'] = note
+
+        print('hihi')
         response = supabase.table("planneddestination").upsert([planned_destination_data]).execute()
 
         if 'error' in response.data:
